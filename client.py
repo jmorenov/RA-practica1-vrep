0dd3c706df12132b25b067c0d9d76096df9a67e4
Javier Moreno <jmorenov28@gmail.com>
2017-10-18 12:34:55 +0200

    Starting with the controller (client.py)

--- client.py
+++ client.py
@@ -3,24 +3,25 @@
 import sys
 import vrep
 import time
-
+import datetime
 
 def read_sonar(clientID, sonar):
     retcode, activated, point, dummy1, dummy2 = vrep.simxReadProximitySensor(clientID, sonar, vrep.simx_opmode_streaming)
     if retcode != vrep.simx_return_ok:
-        print('### Failed reading proximity sensor id=', sonar, retcode)
+        printMessage(clientID, 'Failed reading proximity sensor id= ' + str(sonar) + str(retcode))
     else:
         if activated:
             return point[2]
         else:
             return 1.0
 
+def printMessage(clientID, message):
+    message = '### ' + str(datetime.datetime.now().time()) + ' | ' + message
+    returnCode = vrep.simxAddStatusbarMessage(clientID, message, vrep.simx_opmode_oneshot)
 
-print('### Program started')
+    return returnCode
+
 vrep.simxFinish(-1) # just in case, close all opened connections
-
-# print 'Number of arguments:', len(sys.argv), 'arguments.'
-# print 'Argument List:', str(sys.argv)
 
 port = int(sys.argv[1])
 lmh  = int(sys.argv[2])
@@ -32,15 +33,19 @@ sonar7 = int(sys.argv[7])
 
 # Connect to V-REP
 clientID = vrep.simxStart('127.0.0.1', port, True, True, 2000, 5)
-if clientID == -1:
-    print ('### Failed connecting to remote API server')
-else:
-    # print 'sonar2 =', read_sonar(clientID, sonar2)
-    # print 'sonar4 =', read_sonar(clientID, sonar4)
-    # print 'sonar5 =', read_sonar(clientID, sonar5)
-    # print 'sonar7 =', read_sonar(clientID, sonar7)
+printMessage(clientID, 'Number of arguments: ' + str(len(sys.argv)) + 'arguments.')
+printMessage(clientID, 'Argument List: ' + str(sys.argv))
+printMessage(clientID, 'Program started')
 
-    print ('### Connected to remote API server')
+if clientID == -1:
+    printMessage(clientID, 'Failed connecting to remote API server')
+else:
+    printMessage(clientID, 'sonar2 =' + str(read_sonar(clientID, sonar2)))
+    printMessage(clientID, 'sonar4 =' + str(read_sonar(clientID, sonar4)))
+    printMessage(clientID, 'sonar5 =' + str(read_sonar(clientID, sonar5)))
+    printMessage(clientID, 'sonar7 =' + str(read_sonar(clientID, sonar7)))
+
+    print ('Connected to remote API server')
     while (vrep.simxGetConnectionId(clientID) != -1):
         if read_sonar(clientID, sonar4) < 0.8 or read_sonar(clientID, sonar5) < 0.3:
             lspeed = -1.0
@@ -54,16 +59,17 @@ else:
         else:
             lspeed = +1.2
             rspeed = +0.5
+
         vrep.simxSetJointTargetVelocity(clientID, lmh, lspeed, vrep.simx_opmode_oneshot)
         vrep.simxSetJointTargetVelocity(clientID, rmh, rspeed, vrep.simx_opmode_oneshot)
         time.sleep(0.005)
 
-    # print 'sonar2 =', read_sonar(clientID, sonar2)
-    # print 'sonar4 =', read_sonar(clientID, sonar4)
-    # print 'sonar5 =', read_sonar(clientID, sonar5)
-    # print 'sonar7 =', read_sonar(clientID, sonar7)
+    printMessage(clientID, 'sonar2 =' + str(read_sonar(clientID, sonar2)))
+    printMessage(clientID, 'sonar4 =' + str(read_sonar(clientID, sonar4)))
+    printMessage(clientID, 'sonar5 =' + str(read_sonar(clientID, sonar5)))
+    printMessage(clientID, 'sonar7 =' + str(read_sonar(clientID, sonar7)))
 
     # Close the connection to V-REP
     vrep.simxFinish(clientID)
 
-print ('### Program ended')
+printMessage(clientID, 'Program ended')
