eff7707ee2cdf6b0d6f0078701fb0cf52fc969d3
Javier Moreno <javmorenov@gmail.com>
2017-10-21 13:20:16 +0200

    Added new funcionality to controller

--- client.py
+++ client.py
@@ -5,6 +5,8 @@ import vrep
 import time
 import datetime
 
+PROXIMITY_LIMIT = 0.3
+
 def read_sonar(clientID, sonar):
     retcode, activated, point, dummy1, dummy2 = vrep.simxReadProximitySensor(clientID, sonar, vrep.simx_opmode_streaming)
     if retcode != vrep.simx_return_ok:
@@ -14,6 +16,18 @@ def read_sonar(clientID, sonar):
             return point[2]
         else:
             return 1.0
+
+def read_all_sonar(clientID, maxSensonrs = 16):
+    s = vrep.simxGetObjectGroupData(clientID,
+                vrep.sim_object_proximitysensor_type, 13,
+                vrep.simx_opmode_blocking)
+    r = []
+    for i in range(maxSensonrs):
+        if s[2][2*i] == 1:
+            r.append(s[3][6*i+2])
+        else:
+            r.append(1.0)
+    return r
 
 def printMessage(clientID, message):
     message = '### ' + str(datetime.datetime.now().time()) + ' | ' + message
@@ -26,10 +40,10 @@ vrep.simxFinish(-1) # just in case, close all opened connections
 port = int(sys.argv[1])
 lmh  = int(sys.argv[2])
 rmh  = int(sys.argv[3])
-sonar2 = int(sys.argv[4])
-sonar4 = int(sys.argv[5])
-sonar5 = int(sys.argv[6])
-sonar7 = int(sys.argv[7])
+#sonar2 = int(sys.argv[4])
+#sonar4 = int(sys.argv[5])
+#sonar5 = int(sys.argv[6])
+#sonar7 = int(sys.argv[7])
 
 # Connect to V-REP
 clientID = vrep.simxStart('127.0.0.1', port, True, True, 2000, 5)
@@ -40,34 +54,55 @@ printMessage(clientID, 'Program started')
 if clientID == -1:
     printMessage(clientID, 'Failed connecting to remote API server')
 else:
-    printMessage(clientID, 'sonar2 =' + str(read_sonar(clientID, sonar2)))
-    printMessage(clientID, 'sonar4 =' + str(read_sonar(clientID, sonar4)))
-    printMessage(clientID, 'sonar5 =' + str(read_sonar(clientID, sonar5)))
-    printMessage(clientID, 'sonar7 =' + str(read_sonar(clientID, sonar7)))
-
     print ('Connected to remote API server')
+    oldMsg = msg = ''
     while (vrep.simxGetConnectionId(clientID) != -1):
-        if read_sonar(clientID, sonar4) < 0.8 or read_sonar(clientID, sonar5) < 0.3:
+        dataSonars = read_all_sonar(clientID, 8)
+
+        lspeed = 1.0
+        rspeed = 1.0
+
+        for i in range(len(dataSonars)):
+            if (dataSonars[i] <= PROXIMITY_LIMIT):
+                if (i <= 4):
+                    rspeed = -1.0
+                    lspeed = +2.0
+                else:
+                    lspeed = -1.0
+                    rspeed = +2.0
+                break
+
+        msg = 'left: ' + str(lspeed) + '| right: ' + str(rspeed)
+
+        '''
+        if dataSonars[3] < 0.8 or dataSonars[4] < 0.3:
             lspeed = -1.0
             rspeed = +1.7
-        elif read_sonar(clientID, sonar2) < 0.7:
+            msg = 'turn left'
+        elif dataSonars[1] < 0.7:
             lspeed = +1.2
-            rspeed = +1.5            
-        elif read_sonar(clientID, sonar7) < 0.3:
+            rspeed = +1.5
+            msg = 'recto left'
+        elif dataSonars[6] < 0.3:
             lspeed = +0.1
-            rspeed = +1.4            
+            rspeed = +1.4
+            msg = 'turn left'
         else:
-            lspeed = +1.2
-            rspeed = +0.5
+            lspeed = +1.0
+            rspeed = +1.0
+            msg = 'recto'
+        '''
 
         vrep.simxSetJointTargetVelocity(clientID, lmh, lspeed, vrep.simx_opmode_oneshot)
         vrep.simxSetJointTargetVelocity(clientID, rmh, rspeed, vrep.simx_opmode_oneshot)
-        time.sleep(0.005)
 
-    printMessage(clientID, 'sonar2 =' + str(read_sonar(clientID, sonar2)))
-    printMessage(clientID, 'sonar4 =' + str(read_sonar(clientID, sonar4)))
-    printMessage(clientID, 'sonar5 =' + str(read_sonar(clientID, sonar5)))
-    printMessage(clientID, 'sonar7 =' + str(read_sonar(clientID, sonar7)))
+
+        '''if oldMsg != msg:
+            printMessage(clientID, msg)
+            oldMsg = msg'''
+
+
+        time.sleep(0.005)
 
     # Close the connection to V-REP
     vrep.simxFinish(clientID)
